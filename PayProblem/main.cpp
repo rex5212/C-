c8c380dbd86845bfdc2b8584429981cd0d16a0e9
rex5212 <xrexv158925lpo@gmail.com>
2022-12-25 23:00:59 -0300

    :wrench: code update and improvement :hammer:

--- PayProblem/main.cpp
+++ PayProblem/main.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 // fazer o problema pagamento base e adiciona a verificação de horas trabalhadas (para fica mais exato coloca com segundos)
-// terminar
+// terminar especificação de cada cargo (bonus do cargo) e criar a opcao de voce fazer o seu cargo
 
 int main()
 {
 
     string nome;
 
-    int opcao, horas, minutos, segundos;
+    int opcao, horas, minutos, segundos, horasF, minutosF, segundosF;
+
+    double salario, salario1H, salario1M, salario1S, pagamento;
+
+    cout << fixed << setprecision(2);
 
     cout << "Escreva qual o seu nome: ";
     getline(cin, nome);
@@ -20,32 +24,168 @@ int main()
 
     switch(opcao){
         case 1:
+            cout << "Qual o seu Salario: ";
+            cin >> salario;
             cout << "Quantas horas voce trabalhou: ";
             cin >> horas;
-            cout << "Quantos minutos voce trabalhou: ";
-            cin >> minutos;
-            cout << "Quantos segundos voce trabalhou: ";
-            cin >> segundos;
-            if(horas > 660){
-                pagamento = salario;
-            }
-            if(horas < 660){
-                horasF = 660 - horas;
-                minutosF = 60 - minutos;
-                segundosF = 60 - segundos;
-                pagamento = salario - (((horasF * salario) / horas) + ((minutosF * (salario / 60)) / minutos) + ((segundosF * (salario % 60) / segundos));
-                cout << "Falta " << horas << ":" << minutos << ":" << segundos << " para comprir a carga horaria mensal" << endl;
+            if(horas >= 660){
+                cout << "Sua hora cota de horas foi cumprida funcionario(a) "<< nome << "." << endl << "seu salario foi R$ " << salario;
+            } else {
+                cout << "Quantos minutos voce trabalhou: ";
+                cin >> minutos;
+                cout << "Quantos segundos voce trabalhou: ";
+                cin >> segundos;
+
+
+                // ve certinho como usar if e else ternario
+                if (horas != 0){
+                    horasF = 660 - horas;
+                } else {
+                    horasF = 0;
+                }
+
+                if(minutos != 0){
+                    minutosF = 60 - minutos;
+                } else {
+                    minutosF = 0;
+                }
+
+                if (segundos != 0){
+                    segundosF = 60 - segundos;
+                } else {
+                    segundosF = 0;
+                }
+
+                salario1H = (1 * salario) / 660;
+                salario1M = salario1H / 60;
+                salario1S = salario1M / 60;
+                pagamento = salario - ((salario1H * horasF) + (salario1M * minutosF) + (salario1S * segundosF));
+                cout << "Falta " << horasF << ":" << minutosF << ":" << segundosF << " para comprir a carga horaria mensal, " << nome << endl;
                 cout << "Seu salario desse mes é R$" << pagamento << endl;
             }
                 break;
         case 2:
-            cout << "Coordenador";
+            cout << "Qual o seu Salario: ";
+            cin >> salario;
+            cout << "Quantas horas voce trabalhou: ";
+            cin >> horas;
+            if(horas >= 660){
+                cout << "Sua hora cota de horas foi cumprida funcionario(a) "<< nome << "." << endl << "seu salario foi R$ " << salario;
+            } else {
+                cout << "Quantos minutos voce trabalhou: ";
+                cin >> minutos;
+                cout << "Quantos segundos voce trabalhou: ";
+                cin >> segundos;
+
+
+                // ve certinho como usar if e else ternario
+                if (horas != 0){
+                    horasF = 660 - horas;
+                } else {
+                    horasF = 0;
+                }
+
+                if(minutos != 0){
+                    minutosF = 60 - minutos;
+                } else {
+                    minutosF = 0;
+                }
+
+                if (segundos != 0){
+                    segundosF = 60 - segundos;
+                } else {
+                    segundosF = 0;
+                }
+
+                salario1H = (1 * salario) / 660;
+                salario1M = salario1H / 60;
+                salario1S = salario1M / 60;
+                pagamento = salario - ((salario1H * horasF) + (salario1M * minutosF) + (salario1S * segundosF));
+                cout << "Falta " << horasF << ":" << minutosF << ":" << segundosF << " para comprir a carga horaria mensal, " << nome << endl;
+                cout << "Seu salario desse mes é R$" << pagamento << endl;
+            }
                 break;
         case 3:
-            cout << "Tercerizado";
+            cout << "Qual o seu Salario: ";
+            cin >> salario;
+            cout << "Quantas horas voce trabalhou: ";
+            cin >> horas;
+            if(horas >= 660){
+                cout << "Sua hora cota de horas foi cumprida funcionario(a) "<< nome << "." << endl << "seu salario foi R$ " << salario;
+            } else {
+                cout << "Quantos minutos voce trabalhou: ";
+                cin >> minutos;
+                cout << "Quantos segundos voce trabalhou: ";
+                cin >> segundos;
+
+
+                // ve certinho como usar if e else ternario
+                if (horas != 0){
+                    horasF = 660 - horas;
+                } else {
+                    horasF = 0;
+                }
+
+                if(minutos != 0){
+                    minutosF = 60 - minutos;
+                } else {
+                    minutosF = 0;
+                }
+
+                if (segundos != 0){
+                    segundosF = 60 - segundos;
+                } else {
+                    segundosF = 0;
+                }
+
+                salario1H = (1 * salario) / 660;
+                salario1M = salario1H / 60;
+                salario1S = salario1M / 60;
+                pagamento = salario - ((salario1H * horasF) + (salario1M * minutosF) + (salario1S * segundosF));
+                cout << "Falta " << horasF << ":" << minutosF << ":" << segundosF << " para comprir a carga horaria mensal, " << nome << endl;
+                cout << "Seu salario desse mes é R$" << pagamento << endl;
+            }
                 break;
         case 4:
-            cout << "Diretor";
+            cout << "Qual o seu Salario: ";
+            cin >> salario;
+            cout << "Quantas horas voce trabalhou: ";
+            cin >> horas;
+            if(horas >= 660){
+                cout << "Sua hora cota de horas foi cumprida funcionario(a) "<< nome << "." << endl << "seu salario foi R$ " << salario;
+            } else {
+                cout << "Quantos minutos voce trabalhou: ";
+                cin >> minutos;
+                cout << "Quantos segundos voce trabalhou: ";
+                cin >> segundos;
+
+
+                // ve certinho como usar if e else ternario
+                if (horas != 0){
+                    horasF = 660 - horas;
+                } else {
+                    horasF = 0;
+                }
+
+                if(minutos != 0){
+                    minutosF = 60 - minutos;
+                } else {
+                    minutosF = 0;
+                }
+
+                if (segundos != 0){
+                    segundosF = 60 - segundos;
+                } else {
+                    segundosF = 0;
+                }
+
+                salario1H = (1 * salario) / 660;
+                salario1M = salario1H / 60;
+                salario1S = salario1M / 60;
+                pagamento = salario - ((salario1H * horasF) + (salario1M * minutosF) + (salario1S * segundosF));
+                cout << "Falta " << horasF << ":" << minutosF << ":" << segundosF << " para comprir a carga horaria mensal, " << nome << endl;
+                cout << "Seu salario desse mes é R$" << pagamento << endl;
+            }
                 break;
     }
 
--- TimeProblem/main.cpp
+++ TimeProblem/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// fazer o problema tempo base com a adição de pode escolhe se vai se de segundos || minutos para horas ou dia (ai tem o de horas tambem) !! e o inverso !! - não completado...
+// fazer o problema tempo base com a adição de pode escolhe se vai se de segundos || minutos para horas ou dia (ai tem o de horas tambem) !! e o inverso !! - continua a parte de dia transformação
 
 int main()
 {
@@ -13,9 +13,9 @@ int main()
     // levando mes como tendo 30 dias
 
     cout << "Escolha qual o tipo de dado que ira se transformado: " << endl;
-    cout << "Versao 0.8, por enquanto esta fixo em segundo" << endl;
-    //getline(cin, opcao);
-    opcao = "segundos";
+    //cout << "Versao 0.8, por enquanto esta fixo em segundo" << endl;
+    getline(cin, opcao);
+    //opcao = "horas";
 
     if(opcao == "segundos"){
         cout << "Fale em qual tipo de dados voce que tranformar: ";
@@ -105,90 +105,211 @@ int main()
         }
     }
 
-/*
+
     if(opcao == "minutos"){
         cout << "Fale em qual tipo de dados voce que tranformar: ";
         getline(cin, transformacao);
+        //transformacao = "dia";
         cout << "Quanto minutos voce que transformar: ";
         cin >> minutos;
+        //minutos = 1550;
         if(transformacao == "segundos"){
             segundos = minutos * 60;
-            cout << "Dei " << segundos << " Segundos";
+            cout << "Deu " << segundos << " Segundos";
         } else if(transformacao == "minutos"){
             cout << "Deu " << minutos << " Minutos";
         } else if(transformacao == "horas"){
             if(minutos < 60){
-                cout << "Não da para transforma para minutos, ja que a quantidade de segundos e insuficiente, necessita de pelo menos 60 minutos: " <<  minutos << " Minutos";
+                cout << "Não da para transforma para minutos, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 60 minutos: " <<  minutos << " Minutos";
             } else {
                 horas = minutos / 60;
                 minutos = minutos % 60;
                 resto = minutos % 60;
                 segundos = resto / 60;
-                cout << "Deu " << horas << " Horas, " << minutos << " Minutos e " << segundos << " Segundos";
+                cout << "Deu " << horas << " Horas, " << minutos << " Minutos";
             }
         } else if (transformacao == "dia"){
-            if(segundos < 1440){
-                cout << "Não da para transforma para dias, ja que a quantidade de segundos e insuficiente, necessita de pelo menos 1440 minutos: " <<  minutos << " Minutos";
+            if(minutos < 1440){
+                cout << "Não da para transforma para dias, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 1440 minutos: " <<  minutos << " Minutos";
             } else {
                 dia = minutos / 1440;
                 resto = minutos % 1440;
-                horas = resto / 3600;
-                resto = segundos % 3600;
-                minutos = resto / 60;
-                resto = segundos % 60;
-                segundos = resto % 60;
-                cout << "Deu " << dia << " Dias, " << horas << " Horas,  " << minutos << " Minutos e " << segundos << " Segundos";
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << dia << " Dias, " << horas << " Horas,  " << minutos << " Minutos";
             }
         } else if(transformacao == "semana"){
-            if(segundos < 604800){
-                cout << "Não da para transforma para semanas, ja que a quantidade de segundos e insuficiente, necessita de pelo menos 604800 segundos: " <<  minutos << " Minutos";
+            if(segundos < 10080){
+                cout << "Não da para transforma para semanas, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 10080 minutos: " <<  minutos << " Minutos";
             } else {
-                semanas = segundos / 604800;
-                resto = segundos % 604800;
-                dia = resto / 86400;
-                resto = segundos % 86400;
-                horas = resto / 3600;
-                resto = segundos % 3600;
-                minutos = resto / 60;
-                resto = segundos % 60;
-                segundos = resto % 60;
-                cout << "Deu " << semanas << " Semanas, " << dia << " Dias, " << horas << " Horas,  " << minutos << " Minutos e " << segundos << " Segundos";
+                semanas = minutos / 10080;
+                resto = minutos % 10080;
+                dia = resto / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << semanas << " Semanas, " << dia << " Dias, " << horas << " Horas,  " << minutos << "Minutos";
             }
         } else if(transformacao == "mes"){
-            if(segundos < 2592000){
-                cout << "Não da para transforma para mes, ja que a quantidade de segundos e insuficiente, necessita de pelo menos 2592000 (quando o mesmo for de 30 dias) segundos: " <<  segundos << " Segundos";
+            if(minutos < 43200){
+                cout << "Não da para transforma para mes, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 43200 (quando o mesmo for de 30 dias) Minutos: " <<  minutos << " Minutos";
             } else {
-                mes = segundos / 2592000;
-                resto = segundos % 2592000;
-                dia = resto / 86400;
-                resto = segundos % 86400;
-                horas = resto / 3600;
-                resto = segundos % 3600;
-                minutos = resto / 60;
-                resto = segundos % 60;
-                segundos = resto % 60;
-                cout << "Deu " << mes << " mes(es), " << dia << " Dias, " << horas << " Horas, " << minutos << " Minutos e " << segundos << " Segundos";
+                mes = minutos / 43200;
+                resto = minutos % 43200;
+                dia = resto / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << mes << " mes(es), " << dia << " Dias, " << horas << " Horas, " << minutos << " Minutos";
             }
         } else if(transformacao == "ano"){
-            if(segundos < 31104000){
-                cout << "Não da para transforma para ano, ja que a quantidade de segundos e insuficiente, necessita de pelo menos 31104000 segundos: " <<  segundos << " Segundos";
+            if(minutos < 518400){
+                cout << "Não da para transforma para ano, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 518400 Minutos: " <<  minutos << " Minutos";
             } else {
-                ano = segundos / 31104000;
-                resto = segundos % 31104000;
-                mes = resto / 2592000;
-                resto = segundos % 2592000;
-                dia = resto / 86400;
-                resto = segundos % 86400;
-                horas = resto / 3600;
-                resto = segundos % 3600;
-                minutos = resto / 60;
-                resto = segundos % 60;
-                segundos = resto % 60;
-                cout << "Deu " << ano << " ano(s), "<< mes << " mes(es), " << dia << " Dias, " << horas << " Horas, " << minutos << " Minutos e " << segundos << " Segundos";
+                ano = minutos / 518400;
+                resto = minutos % 518400;
+                mes = resto / 43200;
+                resto = minutos % 43200;
+                dia = resto / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << ano << " ano(s), "<< mes << " mes(es), " << dia << " Dias, " << horas << " Horas, " << minutos << " Minutos";
             }
         }
     }
-*/
+
+    if(opcao == "horas"){
+        cout << "Fale em qual tipo de dados voce que tranformar: ";
+        getline(cin, transformacao);
+        //transformacao = "ano";
+        cout << "Quantas horas voce que transformar: ";
+        cin >> horas;
+        horas = 18296;
+        if(transformacao == "segundos"){
+            segundos = horas * 3600;
+            cout << "Deu " << segundos << " Segundos";
+        } else if(transformacao == "minutos"){
+            minutos = horas * 60;
+            cout << "Deu " << minutos << " Minutos";
+        } else if(transformacao == "horas"){
+            cout << "Deu " << horas << " Horas";
+        } else if (transformacao == "dia"){
+            if(horas < 24){
+                cout << "Não da para transforma para dia, ja que a quantidade de horas e insuficiente, necessita de pelo menos 24 horas: " <<  horas << " Horas";
+            } else {
+                dia = horas / 24;
+                horas = horas % 24;
+                cout << "Deu " << dia << " Dias, " << horas << " Horas";
+            }
+        } else if(transformacao == "semana"){
+            if(horas < 168){
+                cout << "Não da para transforma para semanas, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 168 horas: " <<  horas << " Horas";
+            } else {
+                semanas = horas / 168;
+                resto = horas % 168;
+                dia = resto / 24;
+                horas = resto % 24;
+                cout << "Deu " << semanas << " Semanas, " << dia << " Dias, " << horas << " Horas";
+            }
+        } else if(transformacao == "mes"){
+            if(horas < 720){
+                cout << "Não da para transforma para mes, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 720 (quando o mesmo for de 30 dias) Horas: " <<  horas << " Horas";
+            } else {
+                mes = horas / 720;
+                resto = horas % 720;
+                dia = resto / 24;
+                horas = resto % 24;
+                cout << "Deu " << mes << " mes(es), " << dia << " Dias, " << horas << " Horas";
+            }
+        } else if(transformacao == "ano"){
+            if(horas < 8760){
+                cout << "Não da para transforma para ano, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 8760 Horas: " <<  horas << " Horas";
+            } else {
+                ano = horas / 8760;
+                resto = horas % 8760;
+                mes = resto / 720;
+                resto = horas % 720;
+                dia = resto / 24;
+                horas = resto % 24;
+                cout << "Deu " << ano << " ano(s), "<< mes << " mes(es), " << dia << " Dias, " << horas << " Horas";
+            }
+        }
+    }
+
+    if(opcao == "dia"){
+        cout << "Fale em qual tipo de dados voce que tranformar: ";
+        getline(cin, transformacao);
+        //transformacao = "dia";
+        cout << "Quantas dia voce que transformar: ";
+        cin >> dia;
+        //minutos = 1550;
+        if(transformacao == "segundos"){
+            segundos = horas * 3600;
+            cout << "Deu " << segundos << " Segundos";
+        } else if(transformacao == "minutos"){
+            cout << "Deu " << minutos << " Minutos";
+        } else if(transformacao == "horas"){
+            if(minutos < 60){
+                cout << "Não da para transforma para minutos, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 60 minutos: " <<  minutos << " Minutos";
+            } else {
+                horas = minutos / 60;
+                minutos = minutos % 60;
+                resto = minutos % 60;
+                segundos = resto / 60;
+                cout << "Deu " << horas << " Horas, " << minutos << " Minutos";
+            }
+        } else if (transformacao == "dia"){
+            if(minutos < 1440){
+                cout << "Não da para transforma para dias, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 1440 minutos: " <<  minutos << " Minutos";
+            } else {
+                dia = minutos / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << dia << " Dias, " << horas << " Horas,  " << minutos << " Minutos";
+            }
+        } else if(transformacao == "semana"){
+            if(segundos < 10080){
+                cout << "Não da para transforma para semanas, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 10080 minutos: " <<  minutos << " Minutos";
+            } else {
+                semanas = minutos / 10080;
+                resto = minutos % 10080;
+                dia = resto / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << semanas << " Semanas, " << dia << " Dias, " << horas << " Horas,  " << minutos << "Minutos";
+            }
+        } else if(transformacao == "mes"){
+            if(minutos < 43200){
+                cout << "Não da para transforma para mes, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 43200 (quando o mesmo for de 30 dias) Minutos: " <<  minutos << " Minutos";
+            } else {
+                mes = minutos / 43200;
+                resto = minutos % 43200;
+                dia = resto / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << mes << " mes(es), " << dia << " Dias, " << horas << " Horas, " << minutos << " Minutos";
+            }
+        } else if(transformacao == "ano"){
+            if(minutos < 518400){
+                cout << "Não da para transforma para ano, ja que a quantidade de minutos e insuficiente, necessita de pelo menos 518400 Minutos: " <<  minutos << " Minutos";
+            } else {
+                ano = minutos / 518400;
+                resto = minutos % 518400;
+                mes = resto / 43200;
+                resto = minutos % 43200;
+                dia = resto / 1440;
+                resto = minutos % 1440;
+                horas = resto / 60;
+                minutos = resto % 60;
+                cout << "Deu " << ano << " ano(s), "<< mes << " mes(es), " << dia << " Dias, " << horas << " Horas, " << minutos << " Minutos";
+            }
+        }
+    }
+
 
     return 0;
 }
